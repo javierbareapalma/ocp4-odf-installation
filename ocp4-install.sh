a126ade8f7bb2c5dbf5805fd79b9da963ecb1992
fperearodriguez <fperearodriguez@gmail.com>
2021-11-10 12:13:54 +0100

    update installer

--- ocp4-install.sh
+++ ocp4-install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+set -x
+
 ## Env
 DIR=$(pwd)
 INSTALL="0"
+OCP_VERSION=$1
 
 ## Prerequisites
+echo "If you want to install a specific OCP version, type the version in format X.X"
 echo "Downloading OCP 4 installer if not exists:"
-if [ ! -f ./ocp4-installer.tar.gz ]; then
-    wget -O ./ocp4-installer.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/stable/openshift-install-linux.tar.gz && tar xvzf ./ocp4-installer.tar.gz .
+if [ ! -z $OCP_VERSION ]; then
+    if [ ! -f ./ocp4-installer.tar.gz ]; then
+        echo "OCP VERSION: $OCP_VERSION"
+        REMOTE_OCP_VERSION=$(curl -s https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.6/release.txt | grep Version: | awk '{print $2}')
+        wget -O ./ocp4-installer.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest-4.6/openshift-install-linux-$REMOTE_OCP_VERSION.tar.gz && tar xvzf ./ocp4-installer.tar.gz
+    else
+        echo "Installer exists, using ./ocp4-installer.tar.gz. Unpacking..." ; echo " "
+        tar xvzf ./ocp4-installer.tar.gz
+    fi
 else
-    echo "Installer exists, using ./ocp4-installer.tar.gz. Unpacking..." ; echo " "
-    tar xvzf ./ocp4-installer.tar.gz
+    if [ ! -f ./ocp4-installer.tar.gz ]; then
+        wget -O ./ocp4-installer.tar.gz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux.tar.gz && tar xvzf ./ocp4-installer.tar.gz
+    else
+        echo "Installer exists, using ./ocp4-installer.tar.gz. Unpacking..." ; echo " "
+        tar xvzf ./ocp4-installer.tar.gz
+    fi
 fi
 
 if [ ! -f ./install-dir/terraform.cluster.tfstate ]; then
