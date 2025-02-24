a8e79f69aa90dccd1a3061e614aa8326fd13881d
Javier Barea Palma <jbareapa@redhat.com>
2025-02-24 10:40:22 +0000

    Edit day2tasks.sh, enabling odf-console

--- day2ODF/day2tasks.sh
+++ day2ODF/day2tasks.sh
@@ -1,6 +1,11 @@
+echo "enabling the odf-console plugin"
+oc patch console.operator cluster -n openshift-storage --type json -p '[{"op": "add", "path": "/spec/plugins", "value": ["odf-console"]}]'
+echo "if u have 2 seconds to spare.."
+
 echo "oc -n openshift-storage scale deploy rook-ceph-rgw-ocs-storagecluster-cephobjectstore-a --replicas=2"
 oc -n openshift-storage scale deploy rook-ceph-rgw-ocs-storagecluster-cephobjectstore-a --replicas=2
 echo "if u have 2 seconds to spare.."
+
 echo "oc -n openshift-storage scale deploy noobaa-endpoint --replicas=2"
 oc -n openshift-storage scale deploy noobaa-endpoint --replicas=2
 echo "if u have 2 seconds to spare.."
