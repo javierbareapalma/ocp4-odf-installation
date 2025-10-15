86b25c386be8130dd66994122d47969a8939f089
JAVIER BAREA PALMA <49811752+javierbareapalma@users.noreply.github.com>
2025-10-15 10:37:51 +0200

    Update preday0tasks.sh

--- ODF_kustomize/preday0tasks.sh
+++ ODF_kustomize/preday0tasks.sh
@@ -1,3 +1,10 @@
-nodosworkers=$(oc get node -l node-role.kubernetes.io/worker | grep worker | awk '{print $1}')
+nodosodfworkers=$(oc get node -l node-role.kubernetes.io/worker | grep worker | awk '{print $1}')
 
-for i in $nodosworkers; do oc label node $i cluster.ocs.openshift.io/openshift-storage=''; echo $i; done
+# Label the nodes for ODF storage purpose
+for i in $nodosodfworkers; do oc label node $i  cluster.ocs.openshift.io/openshift-storage=''; echo $i; done
+
+# Assign the ODF nodes to infra role
+#for i in $nodosodfworkers; do oc label node $i  node-role.kubernetes.io/infra=""; done
+
+# Taint the ODF nodes to ensure they are aimed for exclusively for storage, preventing other workloads from being scheduled on them
+#for i in $nodosodfworkers; do oc adm taint node $i  node.ocs.openshift.io/storage="true":NoSchedule; done
